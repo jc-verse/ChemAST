6f03e4d6f6e65a06c06144c6eec712dbcb0b473c
Josh-Cena <sidachen2003@gmail.com>
2021-10-24 12:33:48 +0800

    feat: formula tokenizer
    Signed-off-by: Josh-Cena <sidachen2003@gmail.com>

---
+++ src/tokenizer.ts
@@ -0,0 +1,74 @@
+type Atom = {
+  type: 'atom';
+  value: string;
+};
+
+type Number = {
+  type: 'number';
+  value: number;
+};
+
+type LParen = {
+  type: 'lParen';
+  value: '(' | '[' | '{';
+};
+
+type RParen = {
+  type: 'rParen';
+  value: ')' | ']' | '}';
+};
+
+type Join = {
+  type: 'join';
+  value: '*';
+};
+
+type Token = Atom | Number | LParen | RParen | Join;
+
+function split(formula: string): string[] {
+  const segments = [];
+  for (let i = 0; i < formula.length;) {
+    if (['*', '(', '[', '{', ')', ']', '}'].includes(formula[i])) {
+      segments.push(formula[i]);
+      i++;
+    } else if (/[A-Z]/.test(formula[i])) {
+      if (/[a-z]/.test(formula[i + 1])) {
+        segments.push(formula.substring(i, i + 2));
+        i += 2;
+      } else {
+        segments.push(formula.substring(i, i + 1));
+        i++;
+      }
+    } else if (/[0-9]/.test(formula[i])) {
+      let num = formula[i];
+      i++;
+      while (/[0-9]/.test(formula[i])) {
+        num += formula[i];
+        i++;
+      }
+      segments.push(num);
+    } else if (formula[i] === ' ') {
+      i++;
+    } else {
+      throw new Error(`Parsing Error at position ${i}: '${formula[i]}'`);
+    }
+  }
+  return segments;
+}
+
+export function tokenize(formula: string): Token[] {
+  const segments = split(formula);
+  return segments.map((seg) => {
+    if (seg === '(' || seg === '[' || seg === '{') {
+      return {type: 'lParen', value: seg};
+    } else if (seg === ')' || seg === ']' || seg === '}') {
+      return {type: 'rParen', value: seg};
+    } else if (seg === '*') {
+      return {type: 'join', value: seg};
+    } else if (/\d+/.test(seg)) {
+      return {type: 'number', value: Number(seg)};
+    } else {
+      return {type: 'atom', value: seg};
+    }
+  });
+}
